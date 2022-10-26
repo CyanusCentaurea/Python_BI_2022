4166b6ed5ef5ff8a4745775797799deab5f128d6
Oxana <oxana@LAPTOP-6R3Q1LDI.localdomain>
2022-10-26 18:55:55 +0300

    Fixed return in multiply_matrices, fixed import bugs

--- homework_4/numpy_challenge.py
+++ homework_4/numpy_challenge.py
@@ -1,4 +1,5 @@
-import numpy
+from numpy import array, zeros, linspace, full, copy, arange, dot, shape, linalg, add, transpose, sqrt, float64, reshape
+from numpy import sum as numpy_sum
 
 
 def array_building():
@@ -13,19 +14,19 @@ def array_building():
     [1.  1.5 2.  2.5 3.  3.5]
     using different functions and methods of NumPy.
     """
-    zeros_way1 = numpy.array([0., 0., 0.])
-    zeros_way2 = numpy.zeros(3)
-    zeros_way3 = numpy.linspace(0, 0, num=3)
-    zeros_way4 = numpy.full(3, 0.)
-    zeros_way5 = numpy.full(3, 0, numpy.float64)
-    zeros_way6 = numpy.array([0, 0, 0]).astype(float)
+    zeros_way1 = array([0., 0., 0.])
+    zeros_way2 = zeros(3)
+    zeros_way3 = linspace(0, 0, num=3)
+    zeros_way4 = full(3, 0.)
+    zeros_way5 = full(3, 0, float64)
+    zeros_way6 = array([0, 0, 0]).astype(float)
     zeros_way7 = zeros_way1.copy()
-    small_matrix_way1 = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    small_matrix_way2 = numpy.arange(1, 10).reshape(3, 3)
+    small_matrix_way1 = array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    small_matrix_way2 = arange(1, 10).reshape(3, 3)
     small_matrix_way3 = small_matrix_way1.copy()
-    small_matrix_way4 = numpy.full((3, 3), numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-    by_steps_way1 = numpy.array([1., 1.5, 2., 2.5, 3., 3.5])
-    by_steps_way2 = numpy.arange(1, 4, 0.5)
+    small_matrix_way4 = full((3, 3), array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    by_steps_way1 = array([1., 1.5, 2., 2.5, 3., 3.5])
+    by_steps_way2 = arange(1, 4, 0.5)
     by_steps_way3 = by_steps_way1.copy()
 
 
@@ -33,7 +34,7 @@ def matrix_multiplication(matrix1, matrix2):
     """
     Returns the result of multiplying two matrices. Not adapted for the input of matrices that cannot be multiplied.
     """
-    return numpy.dot(matrix1, matrix2)
+    return dot(matrix1, matrix2)
 
 
 def multiplication_check(matrices):
@@ -54,9 +55,9 @@ def multiply_matrices(matrices):
     res = None
     for matrix1, matrix2 in zip(matrices[:-1], matrices[1:]):
         try:
-            res = numpy.dot(matrix1, matrix2)
+            res = dot(matrix1, matrix2)
         except ValueError:
-            return res
+            return None
     return res
 
 
@@ -65,7 +66,7 @@ def compute_multidimensional_distance(point1, point2):
     Accepts 2 1d-arrays with any number of values (but equal) and returns the distance between them.
     """
     # return numpy.sqrt(numpy.sum(numpy.square(point1 - point2)))
-    return numpy.linalg.norm(point1 - point2)
+    return linalg.norm(point1 - point2)
 
 
 def compute_2d_distance(point1, point2):
@@ -80,7 +81,8 @@ def compute_pair_distances(arr):
     Accepts one 2d array, where each row is an observation, and each column is a feature.
     Returns a matrix of pairwise distances.
     """
-    outer_product = numpy.add.outer(numpy.sum(arr ** 2, axis=1), numpy.sum(arr ** 2, axis=1))
-    mult_matrix = matrix_multiplication(arr, numpy.transpose(arr))
-    return numpy.sqrt(outer_product - 2 * mult_matrix)
+    outer_product = add.outer(numpy_sum(arr ** 2, axis=1), numpy_sum(arr ** 2, axis=1))
+    mult_matrix = matrix_multiplication(arr, transpose(arr))
+    return sqrt(outer_product - 2 * mult_matrix)
+
 
